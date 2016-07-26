4eb433ba032186078abff76b4895381481643cf2
Vinicius Cruz <vinaocruz@gmail.com>
2016-07-25 22:56:23 -0300

    Criado arquivo de routes

--- src/routes.php
+++ src/routes.php
@@ -1,13 +1,5 @@
 <?php
 
 use App\Controller\NotesController;
-use Silex\Application;
 
-date_default_timezone_set($app['app.timezone']);
-
-define("ROOT_PATH", __DIR__ . "/..");
-
-//load routes
 $app->mount($app["api.endpoint"].'/'.$app["api.version"], NotesController::routes($app));
-
-return $app;
--- web/index.php
+++ web/index.php
@@ -2,11 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+define("ROOT_PATH", __DIR__ . "/..");
+
 $app = new Silex\Application();
 
 require __DIR__ . '/../resources/config/main.php';
+
 require __DIR__ . '/../src/middleware.php';
 require __DIR__ . '/../src/providers.php';
-require __DIR__ . '/../src/app.php';
+require __DIR__ . '/../src/routes.php';
 
 $app['http_cache']->run();
