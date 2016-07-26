4a76771b0c993baa648434fd563ccd04428c4570
Vinicius Cruz <vinaocruz@gmail.com>
2016-07-25 22:08:49 -0300

    Organizado arquivos de providers, middleware

---
+++ src/providers.php
@@ -0,0 +1,20 @@
+<?php
+
+use Silex\Provider\HttpCacheServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Carbon\Carbon;
+
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new HttpCacheServiceProvider(), array("http_cache.cache_dir" => ROOT_PATH . "/storage/cache",));
+
+$app->register(new MonologServiceProvider(), array(
+    "monolog.logfile" => ROOT_PATH . "/storage/logs/" . Carbon::now('America/Bahia')->format("Y-m-d") . ".log",
+    "monolog.level" => $app["log.level"],
+    "monolog.name" => "application"
+));
+
+//third party provider
+$app->register(new \App\Provider\NotesServiceProvider());
